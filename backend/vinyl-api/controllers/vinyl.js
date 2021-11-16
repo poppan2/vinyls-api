fe07929389586b7e029c7f852929c16c8c8131b8
poppan2 <ye_aung94@yahoo.com>
2021-11-16 15:08:50 -0800

    modified

--- backend/vinyl-api/controllers/vinyl.js
+++ backend/vinyl-api/controllers/vinyl.js
@@ -1,31 +1,39 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-const Vinyl = require('../models/vinyl')
+const Vinyl = require("../models/vinyl");
 
+router.post("/", (req, res) => {
+  Vinyl.create(req.body)
+    .status(201)
+    .then((vinyl) => {
+      res.json(vinyl);
+    });
+});
 
-router.post('/', (req,res)=>{
-    Vinyl.create(req.body).then((vinyl)=>{
-        res.json(vinyl)
-    })
-    
-})
+router.get("/", (req, res) => {
+  Vinyl.find({}).then((vinyls) => {
+    res.json({
+      status: 200,
+      vinyls: vinyls,
+    });
+  });
+});
 
-router.get('/', (req,res)=>{
-    Vinyl.find({}).then((vinyls)=>{
-        res.json({
-            status: 200,
-            vinyls: vinyls
-        })
-    })
-})
+router.put("/:id", (req, res) => {
+  Vinyl.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .status(200)
+    .then((vinyl) => {
+      res.json(vinyl);
+    });
+});
 
-router.put('/:id', (req,res)=>{
-    res.json('Put Route Works')
-})
-
-router.delete('/:id', (req,res)=>{
-    res.json('Delete Route Works')
-})
-
+//status 204, dont rly need to return anything
+router.delete("/:id", (req, res) => {
+  Vinyl.findByIdAndDelete(req.params.id).then(() => {
+    Vinyl.find({}).then((vinyls) => {
+      res.json(vinyls);
+    });
+  });
+});
 
 module.exports = router;
