a62927920eec9b028cde3dd0f61e737374d3ed69
poppan2 <ye_aung94@yahoo.com>
2021-11-16 16:56:11 -0800

    file created

---
+++ frontend/vinyl-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import VinylForm from './VinylForm'
+function App() {
+  return (
+    <div className="App">
+      Welcome to FrontEnd
+      <VinylForm />
+    </div>
+  );
+}
+
+export default App;
